d881952de202676136c16a8a37b5e16dc4805c2a
Alexey Koran <alexey.koran@gmail.com>
2025-01-21 02:30:29 +0400

    feat: implement `05-merge-styles`

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,51 @@
+const { createReadStream, createWriteStream } = require('node:fs');
+const { readdir, rm } = require('node:fs/promises');
+const { pipeline } = require('node:stream/promises');
+const { join, extname } = require('node:path');
+
+const inputFolder = 'styles';
+const outputFolder = 'project-dist';
+
+const bundleName = 'bundle.css';
+
+const mergeStyles = async () => {
+  const sourcePath = join(__dirname, inputFolder);
+  const destinationPath = join(__dirname, outputFolder);
+
+  const bundleFilePath = join(destinationPath, bundleName);
+
+  await rm(bundleFilePath, { force: true });
+
+  const sourceDirList = await readdir(sourcePath, {
+    withFileTypes: true,
+  });
+
+  const styles = sourceDirList.filter((element) => {
+    const path = join(element.parentPath, element.name);
+    const extension = extname(path);
+
+    return element.isFile() && extension === '.css';
+  });
+
+  styles.forEach(async (cssFile) => {
+    const sourceFilePath = join(sourcePath, cssFile.name);
+
+    const sourceStream = createReadStream(sourceFilePath);
+
+    const destinationStream = createWriteStream(bundleFilePath, {
+      flags: 'a',
+    });
+
+    await pipeline(sourceStream, destinationStream);
+  });
+};
+
+(async () => {
+  try {
+    await mergeStyles();
+
+    console.debug('Success!');
+  } catch (error) {
+    console.error(error);
+  }
+})();
