e2fc70ee436b7856a01dfb5b4dd1367260999ae6
Alexey Koran <alexey.koran@gmail.com>
2025-01-21 01:30:03 +0400

    fix: `04-copy-directory`, remove deleted files in copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,10 +1,27 @@
 const { createReadStream, createWriteStream } = require('node:fs');
-const { mkdir, readdir } = require('node:fs/promises');
+const { mkdir, readdir, rm } = require('node:fs/promises');
 const { pipeline } = require('node:stream/promises');
 const { join } = require('node:path');
 
 const inputFolder = '/files';
 const outputFolder = '/files-copy';
+
+const copyFile = async (sourcePath, destinationPath, file) => {
+  try {
+    const sourceFilePath = join(sourcePath, file);
+    const destinationFilePath = join(destinationPath, file);
+
+    const sourceStream = createReadStream(sourceFilePath);
+    const destinationStream = createWriteStream(destinationFilePath);
+
+    await pipeline(sourceStream, destinationStream);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+
+  return true;
+};
 
 const cp = async () => {
   const dirnamePath = __dirname;
@@ -15,15 +32,16 @@ const cp = async () => {
   await mkdir(destinationPath, { recursive: true });
 
   const files = await readdir(sourcePath);
+  const copyFiles = await readdir(destinationPath);
+
+  const fileToRemove = copyFiles.filter((file) => !files.includes(file));
+
+  fileToRemove.forEach(async (file) => {
+    await rm(join(destinationPath, file));
+  });
 
   files.forEach(async (file) => {
-    const sourceFilePath = join(sourcePath, file);
-    const destinationFilePath = join(destinationPath, file);
-
-    const sourceStream = createReadStream(sourceFilePath);
-    const destinationStream = createWriteStream(destinationFilePath);
-
-    await pipeline(sourceStream, destinationStream);
+    await copyFile(sourcePath, destinationPath, file);
   });
 };
 
