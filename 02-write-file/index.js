b018cb2d84a97780c5b20db86087c6241b11b63a
Alexey Koran <alexey.koran@gmail.com>
2025-01-20 23:34:50 +0400

    feat: implement `02-write-file`

---
+++ 02-write-file/index.js
@@ -0,0 +1,59 @@
+const { createWriteStream } = require('node:fs');
+const { stdin, stdout, exit: exitProcess } = require('node:process');
+const { createInterface } = require('node:readline/promises');
+const { join } = require('node:path');
+
+const welcomeMessage = 'Welcome!\n';
+const farewellMessage = '\nGoodbye!';
+const exitCommand = 'exit';
+
+const fileName = 'text.txt';
+
+const handleExit = (writeableStream) => {
+  console.log(farewellMessage);
+
+  writeableStream.end();
+  exitProcess(0);
+};
+
+const readLine = createInterface({
+  input: stdin,
+  output: stdout,
+  prompt: 'Write to file:',
+});
+
+const write = async () => {
+  console.log(welcomeMessage);
+
+  const path = join(__dirname, fileName);
+
+  const writeableStream = createWriteStream(path, { flags: 'a' });
+
+  readLine.prompt();
+
+  readLine.on('line', async (line) => {
+    try {
+      if (line.trim() === exitCommand) {
+        handleExit(writeableStream);
+      } else {
+        writeableStream.write(`${line}\n`, 'utf8', (err) => {
+          if (err) {
+            console.error('Error writing to file:', err);
+          }
+        });
+      }
+
+      readLine.prompt();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  readLine.on('SIGINT', () => {
+    handleExit(writeableStream);
+  });
+};
+
+(async () => {
+  await write();
+})();
