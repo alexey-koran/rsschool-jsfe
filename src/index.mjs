d465a82306f03207c232d3a1a94eab237512744b
Alexey Koran <alexey.koran@gmail.com>
2025-04-24 12:21:42 +0400

    refactor: use arrays instead of `vocabulary` object

--- src/index.mjs
+++ src/index.mjs
@@ -1,53 +1,32 @@
-const vocabulary = {
-  0: '',
-  1: 'one',
-  2: 'two',
-  3: 'three',
-  4: 'four',
-  5: 'five',
-  6: 'six',
-  7: 'seven',
-  8: 'eight',
-  9: 'nine',
-  10: 'ten',
-  11: 'eleven',
-  12: 'twelve',
-  13: 'thirteen',
-  14: 'fourteen',
-  15: 'fifteen',
-  16: 'sixteen',
-  17: 'seventeen',
-  18: 'eighteen',
-  19: 'nineteen',
-  20: 'twenty',
-  30: 'thirty',
-  40: 'forty',
-  50: 'fifty',
-  60: 'sixty',
-  70: 'seventy',
-  80: 'eighty',
-  90: 'ninety',
-  100: 'hundred',
-};
+const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
 const toReadable = (number) => {
   if (number === 0) {
     return 'zero';
   }
 
-  if (number > 99) {
-    return `${vocabulary[Math.floor(number / 100)]} hundred ${convertNumber(number % 100)}`.trim();
-  }
-
   return convertNumber(number);
 }
 
 const convertNumber = (number) => {
-  if (number <=20) {
-    return vocabulary[number];
+  if (number >= 100) {
+    return `${ones[Math.floor(number / 100)]} hundred ${convertNumber(number % 100)}`.trim();
   }
 
-  return `${vocabulary[number - number % 10]} ${vocabulary[number % 10]}`.trim();
+  if (number <= 9) {
+    return ones[number];
+  }
+
+  if (number <= 19) {
+    return teens[number - 10];
+  }
+
+  const tensValue = Math.floor(number / 10);
+  const units = number % 10;
+
+  return `${tens[tensValue]} ${ones[units]}`.trim();
 };
 
 export { toReadable };
